5db770aa0940ce4b1905a52339ac0aebf14873c8
Eyal Haiman <pizzaeyal@gmail.com>
2024-07-11 13:21:37 +0300

    added new activities and row layout

--- app/src/main/java/com/example/coffeetruckfinalproject2024/CoffeeTruckListView.kt
+++ app/src/main/java/com/example/coffeetruckfinalproject2024/CoffeeTruckListView.kt
@@ -1,11 +1,12 @@
 package com.example.coffeetruckfinalproject2024
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+var coffeeTruckList:ListView? = null
 class CoffeeTruckListView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +17,6 @@ class CoffeeTruckListView : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        coffeeTruckList = findViewById(R.id.lvCoffeeTruckFeed)
     }
 }
