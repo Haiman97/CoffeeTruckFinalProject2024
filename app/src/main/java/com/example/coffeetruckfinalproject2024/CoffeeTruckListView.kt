9f881cb576961ae14824fe287360f23dff4f8b54
Eyal Haiman <pizzaeyal@gmail.com>
2024-07-11 14:13:19 +0300

    Created Row Created Adapter Linked The adapter I have error app crashes related probably to adapter

--- app/src/main/java/com/example/coffeetruckfinalproject2024/CoffeeTruckListView.kt
+++ app/src/main/java/com/example/coffeetruckfinalproject2024/CoffeeTruckListView.kt
@@ -1,7 +1,12 @@
 package com.example.coffeetruckfinalproject2024
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.ListView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,5 +23,27 @@ class CoffeeTruckListView : AppCompatActivity() {
             insets
         }
         coffeeTruckList = findViewById(R.id.lvCoffeeTruckFeed)
+        coffeeTruckList?.adapter = CoffeeTruckListAdapter()
+    }
+    //Adapter Creation
+    class CoffeeTruckListAdapter:BaseAdapter(){
+        override fun getCount(): Int = 20
+        override fun getItem(position: Int): Any {
+            return 0
+        }
+        override fun getItemId(position: Int): Long {
+            return 0
+        }
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var view : View? = null
+            if(convertView == null){
+                view = LayoutInflater.from(parent?.context).inflate(R.layout.coffeetruckrow,parent,false)
+            }
+            //view= view?:convertView
+            val tvRowName :TextView? = view?.findViewById(R.id.tvRowName) ?:
+                convertView?.findViewById(R.id.tvRowName)
+            tvRowName?.text = "Test"
+            return view ?: convertView!!
+        }
     }
 }
